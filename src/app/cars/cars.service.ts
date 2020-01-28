7df55a200e3c44c780ea2c09ad03326b3785899a
Mirosław Malik <mmiro390@gmail.com>
2020-01-28 23:02:21 +0100

    add car service

---
+++ src/app/cars/cars.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import {Observable} from "rxjs";
+import {Car} from "./models/car";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarsService {
+
+  constructor() { }
+}
